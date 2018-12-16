e08f2a790510ac337f3d9ec3d1c9708f73d367a5
Roman Baldaev <roman.baldaev@econophysica.com>
2018-12-16 10:46:37 +0700

    Backend init

---
+++ backend/human_resources_dep/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class HumanResourcesDepConfig(AppConfig):
+    name = 'human_resources_dep'
---
+++ backend/selection_commitee/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class SelectionCommiteeConfig(AppConfig):
+    name = 'selection_commitee'
